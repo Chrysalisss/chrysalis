fc2852bea5e6beb4dad95b46e4aa8ebce8591191
j-heavy <heavy-ddk@yandex.ru>
2018-10-02 16:56:29 +0500

    feature: add render interface

---
+++ src/render.js
@@ -0,0 +1,29 @@
+const render = (node = null, parent = null) => {
+  const mount = parent ? (el => parent.appendChild(el)) : (el => el);
+  if (typeof nodevdom == 'string' || typeof node == 'number') {
+		return mount(document.createTextNode(node));
+  } 
+
+  if (typeof node == 'boolean' || node === null) {
+		return mount(document.createTextNode(''));
+	} 
+
+	if (typeof node == 'object' && typeof node.type == 'function') {
+		return Component.render(node, parent);
+	}
+
+	if (typeof node == 'object' && typeof node.type == 'string') {
+	  const dom = mount(document.createElement(node.type));
+		for (const child of [/* flatten */].concat(...node.children))
+			render(child, dom);
+		for (const prop in node.props)
+			setAttribute(dom, prop, node.props[prop]);
+		return dom;
+	}
+
+	if (Array.isArray(node)) {
+		// hm...
+	}
+}
+
+export { render }
