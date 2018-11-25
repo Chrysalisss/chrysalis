f30b0813620300f221b7c22feae743c9a54f6ee2
j-heavy <heavy-ddk@yandex.ru>
2018-11-25 05:40:39 +0500

    config: update output files

--- rollup.config.js
+++ rollup.config.js
@@ -9,27 +9,50 @@ import { rollup } from 'rollup';
 
 import babel from 'rollup-plugin-babel';
 
-const header = `Chrysalis v0.9.0-β
+const header = `Chrysalis v0.9.0-β 
 Casper Søkol, <%= moment().format('YYYY') %>
 Distributed under the MIT license`
 
 // config
-export default [
+const config = [
   {
-    input: 'src/Chrysalis.js',
+    input: 'src/Chrysalis.js', // es5 + min
+    output: [
+      { name: 'Chrysalis', file: 'dist/chrysalis.umd.min.js', format: 'umd' },
+      { name: 'Chrysalis', file: 'dist/chrysalis.esm.min.js', format: 'esm'}
+    ],
+    moduleName: 'Chrysalis',
+    plugins: [ resolve(), commonjs(), babel(), uglify(), license({ banner: header }) ]
+  },
+  {
+    input: 'src/Chrysalis.js', // es6 + min
+    output: [
+      { name: 'Chrysalis', file: 'dist/chrysalis.es6.umd.min.js', format: 'umd' },
+      { name: 'Chrysalis', file: 'dist/chrysalis.es6.esm.min.js', format: 'esm'}
+    ],
+    moduleName: 'Chrysalis',
+    plugins: [ resolve(), commonjs(), uglify(), license({ banner: header }) ]
+  },
+  {
+    input: 'src/Chrysalis.js', // es5
     output: [
       { name: 'Chrysalis', file: 'dist/chrysalis.umd.js', format: 'umd' },
       { name: 'Chrysalis', file: 'dist/chrysalis.cjs.js', format: 'cjs'},
       { name: 'Chrysalis', file: 'dist/chrysalis.esm.js', format: 'es' }
     ],
     moduleName: 'Chrysalis',
-    plugins: [
-      resolve(), 
-      commonjs(),
-      babel(),
-      /*eslint(),*/
-      uglify(),
-      license({ banner: header })
-    ]
+    plugins: [ resolve(), commonjs(), babel(), license({ banner: header }) ]
+  },
+  {
+    input: 'src/Chrysalis.js', // es6
+    output: [
+      { name: 'Chrysalis', file: 'dist/chrysalis.es6.umd.js', format: 'umd' },
+      { name: 'Chrysalis', file: 'dist/chrysalis.es6.cjs.js', format: 'cjs'},
+      { name: 'Chrysalis', file: 'dist/chrysalis.es6.esm.js', format: 'es' }
+    ],
+    moduleName: 'Chrysalis',
+    plugins: [ resolve(), commonjs(), license({ banner: header }) ]
   }
-];
+]
+
+export default config
