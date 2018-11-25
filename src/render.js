67c89ee9c06260fd7ec881b827edb84c405c9fb6
j-heavy <heavy-ddk@yandex.ru>
2018-11-26 03:32:16 +0500

    fix(vdom): createElement()

--- src/render.js
+++ src/render.js
@@ -1,17 +1,24 @@
-const render = (nodeName, attributes, children) => {
-  const $el = document.createElement(nodeName)
-  const $children = children || []
+const render = (vnode, parentNode) => {
+  let $el
+  const $children = vnode.children || []
 
-  for (let key in attributes) {
-    $el.setAttribute(key, attributes[key])
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode)
   }
 
-  // recursive function to render childs
+  if (typeof vnode.nodeName === 'string') {
+    $el = document.createElement(vnode.nodeName)
+
+    for (let key in vnode.attributes) {
+      $el.setAttribute(key, vnode.attributes[key])
+    }
+  }
+
   $children.forEach(child => {
-    el.appendChild(renderNode(child))
+    $el.appendChild(render(child, vnode.nodeName))
   })
 
-  return $el
+  parentNode.appendChild($el)
 }
 
 export default render
