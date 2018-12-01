86e98a36b6c756b3a22892b81b6cce987cf15d64
j-heavy <heavy-ddk@yandex.ru>
2018-12-01 20:28:44 +0500

    style: some changes

--- src/createElement.js
+++ src/createElement.js
@@ -1,5 +1,8 @@
 // Create element (hyperScrip)
-const h = (nodeName, attributes, ...children) => {
+const h = (nodeName, attributes, children) => {
+	let children = []
+	let len = arguments.length - 2;
+  while ( len-- > 0 ) children[ len ] = arguments[ len + 2 ];
 
   return {
     nodeName,
--- src/render.js
+++ src/render.js
@@ -1,34 +1,12 @@
-/**
- *
- * VNode:
- * @param {string | number | boolean | undefined} createTextNode()
- * @param {object} typical hyperScript like structure, createElement() -> appendChild()
- * @param {function} functional component, VNode()
- *
- * VNode.name:
- * @param {string} createTextNode()
- *
- */
-
 const createVNode = vnode => {
-  let $el
-  const $children = vnode.children || []
-
-  if (typeof vnode === 'string' || 'number' || 'boolean') {
-    $el = document.createTextNode(vnode)
+  if (typeof vnode !== 'object') {
+    return document.createTextNode(vnode);
   }
-
-  if (typeof vnode.nodeName === 'string') {
-    $el = document.createElement(vnode.nodeName)
-
-    for (let key in vnode.attributes) {
-      $el.setAttribute(key, vnode.attributes[key])
-    }
+  const $el = document.createElement(vnode.nodeName);
+  for (let key in vnode.attributes) {
+    $el.setAttribute(key, vnode.attributes[key])
   }
-
-  $children.forEach(child => {
-    $el.appendChild(createVNode(child))
-  })
+  vnode.children.map(createVNode).forEach($el.appendChild.bind($el));
 
   return $el
 }
