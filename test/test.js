bb3a0f20c47b8fe5498a625954cdb75cf3786c7f
j-heavy <heavy-ddk@yandex.ru>
2018-12-11 16:15:12 +0500

    test: add test for h()

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const { h } = require('../dist/chrysalis.umd')
 
-describe('Basic Mocha String Test', () =>  {
+describe("h()", () =>  {
 
- it('empty div', () => {
+  it("h('div', null)", () => {
         const element = h('div', null)
         const result = {
           nodeName: 'div',
@@ -12,4 +12,35 @@ describe('Basic Mocha String Test', () =>  {
         }
         assert.deepEqual(element, result);
     })
+
+  it("h('div', {})", () => {
+      const element = h('div', {})
+      const result = {
+        nodeName: 'div',
+        attributes: {},
+        children: []
+      }
+      assert.deepEqual(element, result);
+  })
+
+  it("h('div', null, 'Hello, world!')", () => {
+    const element = h('div', null, 'Hello, world!')
+    const result = {
+      nodeName: 'div',
+      attributes: {},
+      children: ['Hello, world!']
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('div', {}, 'Hello, world!')", () => {
+    const element = h('div', {}, 'Hello, world!')
+    const result = {
+      nodeName: 'div',
+      attributes: {},
+      children: ['Hello, world!']
+    }
+    assert.deepEqual(element, result);
+  })
+
 })
