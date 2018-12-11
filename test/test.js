62324a74e5f7b394f88890173faf0c9815609417
j-heavy <heavy-ddk@yandex.ru>
2018-12-11 16:42:56 +0500

    test: add test

--- test/test.js
+++ test/test.js
@@ -43,4 +43,91 @@ describe("h()", () =>  {
     assert.deepEqual(element, result);
   })
 
+  it("h('span', null, 23)", () => {
+    const element = h('span', null, 23)
+    const result = {
+      nodeName: 'span',
+      attributes: {},
+      children: [23]
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('span', null, 1 + 17)", () => {
+    const element = h('span', null, 1 + 17)
+    const result = {
+      nodeName: 'span',
+      attributes: {},
+      children: [18]
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('span', null, null)", () => {
+    const element = h('span', null, null)
+    const result = {
+      nodeName: 'span',
+      attributes: {},
+      children: [null]
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('span', null, undefined)", () => {
+    const element = h('span', null, undefined)
+    const result = {
+      nodeName: 'span',
+      attributes: {},
+      children: [undefined]
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('span', { style: 'color: red' }, 'Hello, wold!')", () => {
+    const element = h('span', { style: 'color: red' }, 'Hello, wold!')
+    const result = {
+      nodeName: 'span',
+      attributes: { style: "color: red" },
+      children: ['Hello, wold!']
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('h1', { style: 'color: red', title: 'greeting' }, 'Hello, wold!')", () => {
+    const element = h('h1', { style: 'color: red', title: 'greeting' }, 'Hello, wold!')
+    const result = {
+      nodeName: 'h1',
+      attributes: { style: "color: red", title: 'greeting' },
+      children: ['Hello, wold!']
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('div', { style: 'color: red' }, h('span', { style: 'color: red' }, 'Hello, wold!'))", () => {
+    const element = h('span', { style: 'color: red' }, h('span', { style: 'color: red' }, 'Hello, wold!'))
+    const result = {
+      nodeName: 'span',
+      attributes: { style: "color: red" },
+      children: [{
+        nodeName: 'span',
+        attributes: { style: "color: red" },
+        children: ['Hello, wold!']
+      }]
+    }
+    assert.deepEqual(element, result);
+  })
+
+  it("h('div', { style: 'color: red' }, h('span', null, 'Hello, wold!'))", () => {
+    const element = h('span', { style: 'color: red' }, h('span', null, 'Hello, wold!'))
+    const result = {
+      nodeName: 'span',
+      attributes: { style: "color: red" },
+      children: [{
+        nodeName: 'span',
+        attributes: {},
+        children: ['Hello, wold!']
+      }]
+    }
+    assert.deepEqual(element, result);
+  })
 })
