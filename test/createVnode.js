7ddebe2c07e0f551afd8e8f80af5d54c6c01cfb8
j-heavy <heavy-ddk@yandex.ru>
2018-12-12 23:51:15 +0500

    test: add tests for render()

---
+++ test/createVnode.js
@@ -0,0 +1,23 @@
+const jsdom = require('jsdom')
+const assert = require('assert');
+const { h, render } = require('../dist/chrysalis.umd')
+
+const { JSDOM } = jsdom
+const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+
+global.document = window.document;
+global.window = window;
+
+describe("render()", () =>  {
+  
+  const element = h('div', null, 'Hello, world!')
+
+  it("<div>Hello, world!</div>", () => {
+    const body = document.querySelector("body")
+    render(element, body)
+
+    const result = '<div>Hello, world!</div>'
+
+    assert.deepEqual(global.document.body.innerHTML, result);
+  })
+})
