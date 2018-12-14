f81eb691a8c9fcf8066300f2e2b3444d99455a8f
j-heavy <heavy-ddk@yandex.ru>
2018-12-14 21:01:52 +0500

    tweaks

--- src/createElement.js
+++ src/createElement.js
@@ -1,7 +1,8 @@
 // Create element (hyperScript notation)
 function h(nodeName, attributes) {
-  var children = [], len = arguments.length - 2;
-  while ( len-- > 0 ) children[ len ] = arguments[ len + 2 ];
+  var children = []
+  let len = arguments.length - 2
+  while (len-- > 0) children[len] = arguments[len + 2]
 
   return {
     nodeName,
