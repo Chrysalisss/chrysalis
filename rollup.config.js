ac640b199f03b8d8b68ac97d33a9af59c67d6109
j-heavy <heavy-ddk@yandex.ru>
2018-12-14 21:10:59 +0500

    config(rollup): delete output files

--- rollup.config.js
+++ rollup.config.js
@@ -8,46 +8,15 @@ const header = `Chrysalis v0.10.3-β
 Casper Søkol, <%= moment().format('YYYY') %>
 Distributed under the MIT license`
 
-// config
-const config = [
-  {
-    input: 'src/Chrysalis.js', // es5 + min
-    output: [
-      { name: 'Chrysalis', file: 'dist/chrysalis.umd.min.js', format: 'umd' },
-      { name: 'Chrysalis', file: 'dist/chrysalis.esm.min.js', format: 'esm'}
-    ],
-    moduleName: 'Chrysalis',
-    plugins: [ resolve(), babel(), uglify(), license({ banner: header }) ]
-  },
-  {
-    input: 'src/Chrysalis.js', // es6 + min
-    output: [
-      { name: 'Chrysalis', file: 'dist/chrysalis.es6.umd.min.js', format: 'umd' },
-      { name: 'Chrysalis', file: 'dist/chrysalis.es6.esm.min.js', format: 'esm'}
-    ],
-    moduleName: 'Chrysalis',
-    plugins: [ resolve(), uglify(), license({ banner: header }) ]
-  },
-  {
-    input: 'src/Chrysalis.js', // es5
-    output: [
-      { name: 'Chrysalis', file: 'dist/chrysalis.umd.js', format: 'umd' },
-      { name: 'Chrysalis', file: 'dist/chrysalis.cjs.js', format: 'cjs'},
-      { name: 'Chrysalis', file: 'dist/chrysalis.esm.js', format: 'es' }
-    ],
-    moduleName: 'Chrysalis',
-    plugins: [ resolve(), commonjs(), babel(), license({ banner: header }) ]
-  },
-  {
-    input: 'src/Chrysalis.js', // es6
-    output: [
-      { name: 'Chrysalis', file: 'dist/chrysalis.es6.umd.js', format: 'umd' },
-      { name: 'Chrysalis', file: 'dist/chrysalis.es6.cjs.js', format: 'cjs'},
-      { name: 'Chrysalis', file: 'dist/chrysalis.es6.esm.js', format: 'es' }
-    ],
-    moduleName: 'Chrysalis',
-    plugins: [ resolve(), commonjs(), license({ banner: header }) ]
-  }
-]
+const config = {
+  input: 'src/Chrysalis.js', 
+  output: [
+    { name: 'Chrysalis', file: 'dist/chrysalis.umd.js', format: 'umd' },
+    { name: 'Chrysalis', file: 'dist/chrysalis.cjs.js', format: 'cjs'},
+    { name: 'Chrysalis', file: 'dist/chrysalis.esm.js', format: 'es' }
+  ],
+  moduleName: 'Chrysalis',
+  plugins: [ resolve(), babel(), uglify(), license({ banner: header }) ]
+}
 
 export default config
