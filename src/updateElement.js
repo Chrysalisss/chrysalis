8fd1fab1448066e98cee7fa48d68aa1ea3d6b72f
j-heavy <heavy-ddk@yandex.ru>
2018-12-16 13:58:10 +0500

    fix: change node patching

--- src/updateElement.js
+++ src/updateElement.js
@@ -1,29 +1,32 @@
 import { createVnode } from './render'
 
-// based on deathmood`s code
-const changed = (oldNode, newNode) => {
-  const notObject = typeof oldNode !== 'object'
-  return (
-    typeof oldNode !== typeof newNode || oldNode.nodeName !== newNode.nodeName || (notObject && oldNode !== newNode)
-  )
-}
-
 const updateElement = (parentNode, newNode, oldNode, index = 0) => {
   if (!oldNode) {
     parentNode.appendChild(createVnode(newNode))
-  } else if (!newNode) {
+  }
+
+  if (!newNode) {
     parentNode.removeChild(parentNode.childNodes[index])
-  } else if (changed(newNode, oldNode)) {
+  }
+
+  if (changed(newNode, oldNode)) {
     parentNode.replaceChild(createVnode(newNode), parentNode.childNodes[index])
   } else if (newNode.nodeName) {
     updateAttributes(parentNode.childNodes[index], newNode.attributes, oldNode.attributes)
 
     const length = Math.max(newNode.children.length, oldNode.children.length)
-
-    for (let i = 0; i < length; i++) {
+    let i = -1
+    while (++i < length) {
       updateElement(parentNode.childNodes[index], newNode.children[i], oldNode.children[i], i)
     }
   }
+}
+
+const changed = (newNode, oldNode) => {
+  const notObject = typeof oldNode !== 'object'
+  return (
+    typeof oldNode !== typeof newNode || oldNode.nodeName !== newNode.nodeName || (notObject && oldNode !== newNode)
+  )
 }
 
 const updateAttributes = ($element, newAttrs, oldAttrs = {}) => {
