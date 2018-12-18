8d0a02cb8dedbc5800aed69dbf1d4455f65a344f
j-heavy <heavy-ddk@yandex.ru>
2018-12-18 20:23:31 +0500

    config(package.json): some updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import pkg from './package.json'
+
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify-es'
@@ -5,7 +7,7 @@ import license from 'rollup-plugin-license'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 
-const header = `Chrysalis v0.10.5-β 
+const header = `Chrysalis v${pkg.version}-β 
 Casper Søkol, <%= moment().format('YYYY') %>
 Distributed under the MIT license`
 
