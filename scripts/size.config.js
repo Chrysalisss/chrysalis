cf19b3bcd44d82ec76faa66a29fb0e0168c4314e
j-heavy <heavy-ddk@yandex.ru>
2018-12-20 18:54:38 +0500

    config(rollup): add config for size checker

---
+++ scripts/size.config.js
@@ -0,0 +1,34 @@
+import pkg from '../package.json'
+
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify-es'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+
+const config = {
+  input: 'src/Chrysalis.js',
+  output: { 
+    name: 'Chrysalis', 
+    file: 'dist/chrysalis.esm.js', 
+    format: 'es' 
+  },
+  moduleName: 'Chrysalis',
+  plugins: [
+    resolve(),
+    babel(),
+    uglify(),
+    replace({
+      createVnode$1: 'A',
+      createVnode: 'B',
+      updateAttribute: 'C',
+      updateAttributes: 'D',
+      changed: 'E',
+      nodeName: 'F',
+      props: 'G',
+      children: 'H'
+    })
+  ]
+}
+
+export default config
