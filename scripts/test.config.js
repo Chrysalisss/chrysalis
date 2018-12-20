17859ee208e8420d6e96e43be2049507a5ab6909
j-heavy <heavy-ddk@yandex.ru>
2018-12-20 18:34:48 +0500

    config(rollup): add config for test build

---
+++ scripts/test.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
+const config = {
+  input: 'src/Chrysalis.js',
+  output: { 
+    name: 'Chrysalis', 
+    file: 'dist/chrysalis.umd.js', 
+    format: 'umd' 
+  },
+  moduleName: 'Chrysalis',
+  plugins: [
+    resolve(),
+    babel()
+  ]
+}
+
+export default config
