7d41d771f45b04ffa3f7e0aec5c8f94d35dcea25
j-heavy <heavy-ddk@yandex.ru>
2019-01-02 21:09:39 +0500

    refactor: use 'for ... in ...'  instead of map() reduce size yes, again

--- src/updateAttributes.js
+++ src/updateAttributes.js
@@ -2,17 +2,13 @@ import merge from './utill'
 
 function updateAttrs($element, newAttrs, oldAttrs) {
   // putting attributes together and iterating
-  Object.keys(merge(newAttrs, oldAttrs)).map(name => {
-    if (name == 'class') {
-      // TBD
-    }
-
+  for (let name in merge(newAttrs, oldAttrs)) {
     if (!newAttrs[name]) {
       $element.removeAttribute(name)
     } else if (!oldAttrs[name] || newAttrs[name] !== oldAttrs[name]) {
       $element.setAttribute(name, newAttrs[name])
     }
-  })
+  }
 }
 
 export default updateAttrs
