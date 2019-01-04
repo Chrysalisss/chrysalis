4987c53383fa990ab4dff18e199b62cb6a5ce004
j-heavy <heavy-ddk@yandex.ru>
2019-01-04 14:28:00 +0500

    refactor: improve updateAttrs()

--- src/updateAttributes.js
+++ src/updateAttributes.js
@@ -4,9 +4,18 @@ function updateAttrs($element, newAttrs, oldAttrs) {
   // putting attributes together and iterating
   for (let name in merge(newAttrs, oldAttrs)) {
     if (!newAttrs[name]) {
-      $element.removeAttribute(name)
-    } else if (!oldAttrs[name] || newAttrs[name] !== oldAttrs[name]) {
-      $element.setAttribute(name, newAttrs[name])
+      if (name === 'class') {
+        $el.removeAttribute('class')
+      } else {
+        $el[name] = null
+        delete $el[name]
+      }
+    } else if (newAttrs[name] !== oldAttrs[name]) {
+      if (name === 'class') {
+        $el.setAttribute('class', newAttrs[name])
+      } else {
+        $el[name] = newAttrs[name]
+      }
     }
   }
 }
