7e72ff1020ea7fd9814f05b59f1bd1c0efef6d47
j-heavy <heavy-ddk@yandex.ru>
2019-01-06 23:21:03 +0500

    tweaks

--- src/updateAttributes.js
+++ src/updateAttributes.js
@@ -4,14 +4,14 @@ function updateAttrs($element, newAttrs, oldAttrs) {
   // putting attributes together and iterating
   for (let name in merge(newAttrs, oldAttrs)) {
     if (!newAttrs[name]) {
-      if (name === 'class') {
+      if (name == 'class') {
         $element.removeAttribute('class')
       } else {
         $element[name] = null
         delete $element[name]
       }
-    } else if (newAttrs[name] !== oldAttrs[name]) {
-      if (name === 'class') {
+    } else if (newAttrs[name] != oldAttrs[name]) {
+      if (name == 'class') {
         $element.setAttribute('class', newAttrs[name])
       } else {
         $element[name] = newAttrs[name]
