45ea28f883adfd75109a8b94cd3d07af3961c991
j-heavy <heavy-ddk@yandex.ru>
2019-02-15 19:41:13 +0500

    test: update for new API

--- dist/chrysalis.umd.js
+++ dist/chrysalis.umd.js
@@ -1,7 +1,156 @@
-/**
- * Chrysalis v0.11.0-β
- * Casper Søkol, 2018
- * Distributed under the MIT license
- */
+;(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? factory(exports)
+    : typeof define === 'function' && define.amd
+    ? define(['exports'], factory)
+    : factory((global.Chrysalis = {}))
+})(this, function(exports) {
+  'use strict'
 
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t(e.Chrysalis={})}(this,function(e){function t(e,t){for(var n=[],o=arguments.length-2;o-- >0;)n[o]=arguments[o+2];return"function"==typeof e?e(t):{F:e,G:t||{},H:n}}function n(e,t,n){void 0===n&&(n={}),Object.keys(Object.assign(t,n)).map(function(o){t[o]?n[o]&&t[o]===n[o]||e.setAttribute(o,t[o]):e.removeAttribute(o)})}function o(e,t){if("object"!=typeof e)return document.createTextNode(e);var i=(t=t||"svg"==e.F)?document.createElementNS("http://www.w3.org/2000/svg",e.F):document.createElement(e.F);return n(i,e.G),e.H.map(function(e){return i.appendChild(o(e,t))}),i}function i(e,t,n){t.appendChild(o(e)),void 0!==n&&n()}function d(e,t,i,r,f){if(i||e.appendChild(o(t,f)),t)if(typeof t!=typeof i||t.F!==i.F||"object"!=typeof i&&i!==t)e.replaceChild(o(t,f),e.childNodes[r||0]);else{n(e.childNodes[r||0],t.G,i.G);for(var c=Math.max(t.H.length,i.H.length),u=-1;++u<c;)d(e.childNodes[r||0],t.H[u],i.H[u],u,f=f||"svg"==t.F)}else e.removeChild(e.childNodes[r||0])}e.h=t,e.render=i,e.updateElement=d,Object.defineProperty(e,"__esModule",{value:!0})});
+  /**
+   * JSX/hyperscript notation
+   * Creates a virtual DOM node
+   *
+   * @see https://facebook.github.io/jsx & https://github.com/hyperhype/hyperscript
+   */
+  function h(nodeName, props) {
+    var children = []
+    var len = arguments.length - 2
+
+    while (len-- > 0) {
+      children[len] = arguments[len + 2]
+    } // nodeName is a function -> it`s a component
+
+    if (typeof nodeName === 'function') {
+      return nodeName(props)
+    }
+
+    if (Array.isArray(children[0])) {
+      children = children[0]
+    }
+
+    return {
+      nodeName: nodeName,
+      props: props || {},
+      children: children
+    }
+  }
+
+  function merge(a, b) {
+    var src = {}
+
+    for (var i in a) {
+      src[i] = a[i]
+    }
+
+    for (var i in b) {
+      src[i] = b[i]
+    }
+
+    return src
+  }
+
+  function updateAttrs($element, newAttrs, oldAttrs) {
+    // putting attributes together and iterating
+    for (var name in merge(newAttrs, oldAttrs)) {
+      if (!newAttrs[name]) {
+        if (name == 'class') {
+          $element.removeAttribute('class')
+        } else {
+          $element[name] = null
+          delete $element[name]
+        }
+      } else if (newAttrs[name] != oldAttrs[name]) {
+        if (name == 'class') {
+          $element.setAttribute('class', newAttrs[name])
+        } else {
+          $element[name] = newAttrs[name]
+        }
+      }
+    }
+  }
+
+  function createVnode(vnode, isSVG) {
+    if (typeof vnode !== 'object') {
+      return document.createTextNode(vnode)
+    }
+
+    var element = (isSVG = isSVG || vnode.nodeName == 'svg')
+      ? document.createElementNS('http://www.w3.org/2000/svg', vnode.nodeName)
+      : document.createElement(vnode.nodeName) // props (not attributes) by this time are already applied to the vnode
+
+    updateAttrs(element, vnode.props, {})
+
+    for (var child in vnode.children) {
+      element.appendChild(createVnode(vnode.children[child], isSVG))
+    }
+
+    return element
+  }
+
+  /**
+   * Comparison algorithm DOM and Virtual DOM
+   * Lead time ~O(n^3)
+   */
+
+  function updateElement(newNode, oldNode, element, index, isSVG) {
+    var parentNode = element || ROOT_ELEMENT
+
+    if (oldNode == null) {
+      parentNode.appendChild(createVnode(newNode, isSVG))
+    } else if (newNode == null) {
+      parentNode.removeChild(parentNode.childNodes[index || 0])
+    } else if (notSameNode(newNode, oldNode)) {
+      parentNode.replaceChild(createVnode(newNode, isSVG), parentNode.childNodes[index || 0])
+    } else if (newNode.nodeName) {
+      updateAttrs(parentNode.childNodes[index || 0], newNode.props, oldNode.props)
+      var length = Math.max(newNode.children.length, oldNode.children.length)
+
+      for (var i = -1; ++i < length; ) {
+        updateElement(
+          newNode.children[i],
+          oldNode.children[i],
+          parentNode.childNodes[index || 0],
+          i,
+          (isSVG = isSVG || newNode.nodeName == 'svg')
+        )
+      }
+    }
+  }
+  /**
+   * Node change detection algo
+   * Based on Snabbdom`s algorithm
+   */
+
+  function notSameNode(a, b) {
+    return typeof a !== typeof b || a.nodeName !== b.nodeName || (typeof b !== 'object' && b !== a)
+  }
+
+  var ROOT_ELEMENT // vnode representation of current DOM
+
+  var currentNode
+
+  function start(parentNode, callback) {
+    ROOT_ELEMENT = parentNode
+    updateElement(App(), currentNode, parentNode)
+    currentNode = App()
+
+    if (callback) {
+      callback()
+    }
+  }
+
+  function setState(fn) {
+    if (fn) {
+      fn()
+    }
+
+    start(ROOT_ELEMENT)
+  }
+
+  exports.h = h
+  exports.start = start
+  exports.setState = setState
+
+  Object.defineProperty(exports, '__esModule', { value: true })
+})
--- test/render.js
+++ test/render.js
@@ -1,131 +1,36 @@
 const jsdom = require('jsdom')
 const assert = require('assert')
-const { h, render } = require('../dist/chrysalis.umd')
+const { h, start } = require('../dist/chrysalis.umd')
 
 const { JSDOM } = jsdom
 const { window } = new JSDOM('<!doctype html><html><body><div id="app"></div></body></html>')
 
 global.document = window.document
 global.window = window
-global.navigator = { userAgent: 'node.js' }
 
-describe('render()', () => {
-  const app = document.querySelector('#app')
+describe('start()', () => {
+  const container = document.querySelector('#app')
 
   const deleteNodes = () => {
-    while (app.firstChild) {
-      app.removeChild(app.firstChild)
+    while (container.firstChild) {
+      container.removeChild(container.firstChild)
     }
   }
 
-  it('<div></div>', () => {
-    const element = <div />
-    render(element, app)
+  it('<div><span>Hello World!</span></div>', () => {
+    global.App = () => {
+      return (
+        <div>
+          <span>Hello World!</span>
+        </div>
+      )
+    }
 
-    const result = '<div></div>'
+    start(container)
 
-    assert.equal(app.innerHTML, result)
+    const result = '<div><span>Hello World!</span></div>'
 
-    deleteNodes()
-  })
-
-  it('<div>Hello, world!</div>', () => {
-    const element = <div>Hello, world!</div>
-    render(element, app)
-
-    const result = '<div>Hello, world!</div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div class="greeting">Hello, world!</div>', () => {
-    const element = <div class="greeting">Hello, world!</div>
-    render(element, app)
-
-    const result = '<div class="greeting">Hello, world!</div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div class="greeting" style="color: red">Hello, world!</div>', () => {
-    const element = (
-      <div class="greeting" style="color: red">
-        Hello, world!
-      </div>
-    )
-    render(element, app)
-
-    const result = '<div class="greeting" style="color: red">Hello, world!</div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div><h1>Hello, world!</h1></div>', () => {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    )
-    render(element, app)
-
-    const result = '<div><h1>Hello, world!</h1></div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div><h1 class="greeting" style="color: red">Hello, world!</h1></div>', () => {
-    const element = (
-      <div>
-        <h1 class="greeting" style="color: red">
-          Hello, world!
-        </h1>
-      </div>
-    )
-    render(element, app)
-
-    const result = '<div><h1 class="greeting" style="color: red">Hello, world!</h1></div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div><h1 class="greeting" style="color: red">6</h1></div>', () => {
-    const element = (
-      <div>
-        <h1 class="greeting" style="color: red">
-          6
-        </h1>
-      </div>
-    )
-    render(element, app)
-
-    const result = '<div><h1 class="greeting" style="color: red">6</h1></div>'
-
-    assert.equal(app.innerHTML, result)
-
-    deleteNodes()
-  })
-
-  it('<div><h1 style="color: red">undefined</h1></div>', () => {
-    const element = (
-      <div>
-        <h1 style="color: red">undefined</h1>
-      </div>
-    )
-    render(element, app)
-
-    const result = '<div><h1 style="color: red">undefined</h1></div>'
-
-    assert.equal(app.innerHTML, result)
+    assert.equal(container.innerHTML, result)
 
     deleteNodes()
   })
