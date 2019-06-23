fc3451416ec37280a4ade1d78068debee99e12ea
j-heavy <heavy-ddk@yandex.ru>
2019-06-23 22:41:42 +0500

    perf(component): improve creation algo

--- src/createElement.js
+++ src/createElement.js
@@ -1,4 +1,43 @@
 import updateAttrs from './updateAttributes'
+import { ROOT_ELEMENT } from './start'
+import { merge } from './utill'
+
+const elements = ROOT_ELEMENT.querySelectorAll('*')
+
+function getNode(node) {
+  for (let i = 0; i <= elements.length; i++) {
+    if (elements[i] === node) {
+      return i
+    }
+  }
+}
+
+function createComponent(component, props) {
+  if (typeof component.state === 'function') {
+    component.state = component.state(props)
+  }
+
+  merge(component, {
+    setState(state) {
+      if (typeof state === 'function') {
+        state = state(component.state)
+      }
+
+      for (let key in state) {
+        component.state[key] = state[key]
+      }
+
+      component._update()
+    },
+    _update() {
+      const index = getNode(component._element)
+
+      patch(elements[index - 1], elements[index], component._vnode, (component._vnode = component.render()))
+    },
+    _vnode: component.render(),
+    _element: createElement(component.render())
+  })
+}
 
 function createElement(node, isSVG) {
   if (typeof node !== 'object') {
