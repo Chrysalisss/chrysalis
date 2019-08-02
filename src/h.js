d238f88b36e29b5139c952b079e6578fb4016e1e
j-heavy <heavy-ddk@yandex.ru>
2019-08-03 01:26:52 +0500

    perf: some optimizations

--- src/h.js
+++ src/h.js
@@ -7,11 +7,11 @@
  * css-tricks.com/what-does-the-h-stand-for-in-vues-render-method/
  */
 
-import { NULL, FUNCTION, LENGTH } from './helpers/index'
+import { NULL, FUNCTION, LENGTH, EMPTY_OBJ } from './helpers/index'
 
 function h(name, props) {
-  for (var node, rest = [], children = [], i = arguments[LENGTH]; i-- > 2; ) {
-    rest.push(arguments[i])
+  for (var node, rest = [], children = [], args = arguments, i = args[LENGTH]; i-- > 2; ) {
+    rest.push(args[i])
   }
 
   while (rest[LENGTH] > 0) {
@@ -25,7 +25,7 @@ function h(name, props) {
     }
   }
 
-  props = props || {}
+  props = props || EMPTY_OBJ
 
   if (typeof name == FUNCTION) {
     props.children = children
