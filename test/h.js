fc1758c132b82947e8ffaad4d0e923027d1b86d3
j-heavy <heavy-ddk@yandex.ru>
2019-08-03 01:03:49 +0500

    test: update h() tests

--- test/h.js
+++ test/h.js
@@ -13,7 +13,7 @@ describe('h()', () => {
   it('#1', () => {
     const element = h('div', null)
     const result = {
-      type: 'div',
+      name: 'div',
       props: {},
       children: []
     }
@@ -23,7 +23,7 @@ describe('h()', () => {
   it('#2', () => {
     const element = <div id="main"/>
     const result = {
-      type: 'div',
+      name: 'div',
       props: { id: 'main' },
       children: []
     }
@@ -33,7 +33,7 @@ describe('h()', () => {
   it('#3', () => {
     const element = <div>Hello, world!</div>
     const result = {
-      type: 'div',
+      name: 'div',
       props: {},
       children: ['Hello, world!']
     }
@@ -43,7 +43,7 @@ describe('h()', () => {
   it('#4', () => {
     const element = <div />
     const result = {
-      type: 'div',
+      name: 'div',
       props: {},
       children: []
     }
@@ -53,7 +53,7 @@ describe('h()', () => {
   it('#5', () => {
     const element = <span>23</span>
     const result = {
-      type: 'span',
+      name: 'span',
       props: {},
       children: [23]
     }
@@ -63,7 +63,7 @@ describe('h()', () => {
   it('#6', () => {
     const element = h('span', {}, 1 + 17)
     const result = {
-      type: 'span',
+      name: 'span',
       props: {},
       children: [18]
     }
@@ -73,7 +73,7 @@ describe('h()', () => {
   it('#7', () => {
     const element = <span>{null}</span>
     const result = {
-      type: 'span',
+      name: 'span',
       props: {},
       children: []
     }
@@ -83,7 +83,7 @@ describe('h()', () => {
   it('#8', () => {
     const element = <span>{undefined}</span>
     const result = {
-      type: 'span',
+      name: 'span',
       props: {},
       children: []
     }
@@ -93,7 +93,7 @@ describe('h()', () => {
   it('#9', () => {
     const element = <span style={{ color: 'red', fontSize: '24px'}}>Hello, world!</span>
     const result = {
-      type: 'span',
+      name: 'span',
       props: { style: { color: 'red', fontSize: '24px'} },
       children: ['Hello, world!']
     }
@@ -103,7 +103,7 @@ describe('h()', () => {
   it('#10', () => {
     const element = <h1 title="greeting">Hello, world!</h1>
     const result = {
-      type: 'h1',
+      name: 'h1',
       props: { title: 'greeting' },
       children: ['Hello, world!']
     }
@@ -114,11 +114,11 @@ describe('h()', () => {
     const styles = { color: 'red' }
     const element = <div style={styles}><span style={styles}>Hello, wold!</span></div>
     const result = {
-      type: 'div',
+      name: 'div',
       props: { style: { color: 'red' } },
       children: [
         {
-          type: 'span',
+          name: 'span',
           props: { style: { color: 'red' } },
           children: ['Hello, wold!']
         }
@@ -130,11 +130,11 @@ describe('h()', () => {
   it('#12', () => {
     const element = <div style={{ color: 'red' }}><span>Hello, wold!</span></div>
     const result = {
-      type: 'div',
+      name: 'div',
       props: { style: { color: 'red' } },
       children: [
         {
-          type: 'span',
+          name: 'span',
           props: {},
           children: ['Hello, wold!']
         }
