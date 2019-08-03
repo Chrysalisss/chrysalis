56c6c3fd40cbe57dbea3c9991cf7e5f1e740c0c4
j-heavy <heavy-ddk@yandex.ru>
2019-08-03 23:00:36 +0500

    fix: this is undefined - oncreate hook

--- src/createElement.js
+++ src/createElement.js
@@ -86,7 +86,7 @@ function createElement(node, hooks, isSVG) {
 
   if (node.name[RENDER]) {
     if (node.name[ONCREATE]) {
-      hooks.push(node.name[ONCREATE].bind(this))
+      hooks.push(node.name[ONCREATE].bind(node.name))
     }
 
     node[PROPS][CHILDREN] = node.childNodes
