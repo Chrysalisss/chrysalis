3e00259402f4f6e40e67c59a2ceaa76bdaacb87f
j-heavy <heavy-ddk@yandex.ru>
2019-08-04 19:30:37 +0500

    feature: fragments

--- src/Chrysalis.js
+++ src/Chrysalis.js
@@ -1,9 +1,11 @@
 import h from './h'
 import { render } from './render'
 import { createRef } from './refs'
+import Fragment from './fragment'
 
 export {
 	h, 
 	render, 
-  createRef
+  createRef,
+  Fragment
 }
---
+++ src/fragment.js
@@ -0,0 +1,5 @@
+function Fragment(props) {
+	return props.children
+}
+
+export default Fragment
