d2df1df1ee6577ff30dd62d55bcd997a9fc68677
j-heavy <heavy-ddk@yandex.ru>
2019-08-10 21:19:42 +0500

    config(header): remove useless

--- scripts/build.config.js
+++ scripts/build.config.js
@@ -8,7 +8,7 @@ import license from 'rollup-plugin-license'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 
-const header = `Chrysalis v${pkg.version}-β 
+const header = `Chrysalis v${pkg.version} 
 Casper Søkol, <%= moment().format('YYYY') %>
 Distributed under the MIT license`
 
